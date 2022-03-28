d2b2d00c783724374d70ae58d5f2d593d0400ec6
Derek Lee <dereklee0310@gmail.com>
2022-03-28 17:46:57 +0800

    edit setvbuf

--- hw3/fileperf.c
+++ hw3/fileperf.c
@@ -6,11 +6,7 @@
 
 #define DEFAULT_SIZE 1024
 
-void setLineBuf(FILE *src, FILE *dest, char *srcBuf, char *destBuf);
-
-void setNoBuf(FILE *src, FILE *dest);
-
-void setFullBuf(FILE *src, FILE *dest, char *srcBuf, char *destBuf, int bufSize);
+void setBuf(FILE *src, FILE *dest, int bufSize);
 
 int main(int argc, char *argv[])
 {   
@@ -18,58 +14,42 @@ int main(int argc, char *argv[])
         fprintf(stderr, "number of arguments incorrect");
         exit(1);
     }
-
-
     FILE *src = fopen(argv[1], "r");
     FILE *dest = fopen(argv[2], "w+");
     int bufSize = atoi(argv[3]);
 
-    char *srcBuf = NULL;
-    char *destBuf = NULL;
-    switch(bufSize) {
-        case -1:
-            setLineBuf(src, dest, srcBuf, destBuf);
-            break;
-        case 0:
-            setNoBuf(bufSize);
-            break;
-        default:
-            setFullBuf(src, dest, srcBuf, destBuf, bufSize);
-            break;
+    if(bufSize < -1) {
+        fprintf(stderr, "size of buf needs to be greater or equal to -1");
+        exit(1);
     }
+    setBuf(bufSize);
 
     char wordBuf[80];
 
-    while(true) {
-
+    while() {
+        
     }
 
-    free(srcBuf);
-    free(destBuf);
     fclose(src);
     fclose(dest);
 
     return 0;
 }
 
-void setLineBuf(FILE *src, FILE *dest, char *srcBuf, char *destBuf)
+void setBuf(FILE *src, FILE *dest, int bufSize)
 {
-    srcBuf = (char *)malloc(sizeof(char) * DEFAULT_SIZE);
-    destBuf = (char *)malloc(sizeof(char) * DEFAULT_SIZE);
-    setvbuf(src, srcBuf, _IOLBF, DEFAULT_SIZE);
-    setvbuf(dest, destBuf, _IOLBF, DEFAULT_SIZE);
-}
-
-void setNoBuf(FILE *src, FILE *dest)
-{
-    setvbuf(src, NULL, _IONBF, DEFAULT_SIZE);
-    setvbuf(dest, NULL, _IONBF, DEFAULT_SIZE);
-}
-
-void setFullBuf(FILE *src, FILE *dest, char *srcBuf, char *destBuf, int bufSize)
-{   
-    srcBuf = (char *)malloc(sizeof(char) * bufSize);
-    destBuf = (char *)malloc(sizeof(char) * bufSize);
-    setvbuf(src, buf, _IOFBF, bufSize);
-    setvbuf(dest, buf, _IOFBF, bufSize);
+    switch(bufSize) {
+        case -1:
+            setvbuf(src, NULL, _IOLBF, DEFAULT_SIZE);
+            setvbuf(dest, NULL, _IOLBF, DEFAULT_SIZE);
+            break;
+        case 0:
+            setvbuf(src, NULL, _IONBF, DEFAULT_SIZE);
+            setvbuf(dest, NULL, _IONBF, DEFAULT_SIZE);
+            break;
+        default:
+            setvbuf(src, NULL, _IOFBF, bufSize);
+            setvbuf(dest, NULL, _IOFBF, bufSize);
+            break;
+    }
 }
